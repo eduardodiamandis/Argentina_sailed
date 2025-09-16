8eb243396559513764478f1b978d996f4d1948be
eduardodiamandis <eduardodiamandis080@gmail.com>
2025-09-16 16:30:50 -0300

    - now loggs are send by e-mail,when an error happens - .env = To make it work, an .env file was created that defines the environment variables for the email that will be used to send and the application password generated for access.

--- main.py
+++ main.py
@@ -1,44 +1,30 @@
 from sailde import process_file
 from logger_config import setup_logger
-import os
 
 if __name__ == "__main__":
     logger = setup_logger()
-
     logger.info("Início do processo")
 
-    # URLs
-    Line_Up = 'https://boletines.nabsa.com.ar/c/gr6gdj/lmt8mdy1/mekudkcidae'
-    Sailed = 'https://boletines.nabsa.com.ar/c/gr6gdj/lmt8mdy1/mlod-sfb1uu'
-
-    # Pastas de backup
-    backup_sailed = os.path.join(os.path.expanduser('~'), 'Desktop', 'Argentina', 'Sailed', 'backup', 'sailed_vessel')
-    backup_line_up = os.path.join(os.path.expanduser('~'), 'Desktop', 'Argentina', 'Sailed', 'backup', 'line_vessel')
-
-    sailed = {
-        'url': Sailed,
-        'file_name': 'vessels_sailed_update.xlsx',
-        'destination_path': backup_sailed,
-        'timeout': 40
-    }
-
-    line_up = {
-        'url': Line_Up,
-        'file_name': 'vessel_update.xlsx',
-        'destination_path': backup_line_up,
-        'timeout': 40
-    }
+    try:
+        logger.info("Processando Sailed...")
+        process_file(
+            url="https://boletines.nabsa.com.ar/c/gr6gdj/lmt8mdy1/mlod-sfb1uu",
+            file_name="vessels_sailed_update.xlsx",
+            destination_path=r"C:\Users\Visitante\Desktop\Argentina\Sailed\backup\sailed_vessel",
+            timeout=40
+        )
+    except Exception as e:
+        logger.error(f"Erro processando Sailed: {e}")
 
     try:
-        logger.info("Processando Sailed")
-        process_file(**sailed)
-    except Exception as error:
-        logger.error(f"Erro processando Sailed: {error}")
-
-    try:
-        logger.info("Processando Line_Up")
-        process_file(**line_up)
-    except Exception as error:
-        logger.error(f"Erro processando Line_Up: {error}")
+        logger.info("Processando Line_Up...")
+        process_file(
+            url="https://boletines.nabsa.com.ar/c/gr6gdj/lmt8mdy1/mekudkcidae",
+            file_name="vessel_update.xlsx",
+            destination_path=r"C:\Users\Visitante\Desktop\Argentina\Sailed\backup\line_vessel",
+            timeout=40
+        )
+    except Exception as e:
+        logger.error(f"Erro processando Line_Up: {e}")
 
     logger.info("Processo finalizado")
